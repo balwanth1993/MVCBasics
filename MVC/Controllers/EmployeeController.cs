2166e72acaad3438c2c3a4559e90de8ab104a1e8
balwanth1993 <balwanth.r93@gmail.com>
2016-12-03 18:16:52 -0600

    Add project files.

---
+++ MVC/Controllers/EmployeeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+       
+        public ActionResult Details()
+        {
+            Employee emp = new Employee()
+            {
+                Id = 1,Name= "Balwanth", City="Hyderabad",Gender="Male"
+            };
+            return View(emp);
+        }
+        public ActionResult Account()
+        {
+            return Redirect("~/Home/Details");
+        }
+        public ActionResult Partial()
+        {
+            return PartialView("Partial","Employee");
+        }
+        public ActionResult Content()
+        {
+            return Content();
+        }
+        public ActionResult File()
+        {
+            return File();
+        }
+    }
+}
---
+++ MVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.countries = new List<string>
+            {
+                "India",
+                "Us",
+                "UK",
+                "Australia"
+            };
+            return View();
+        }
+        
+    }
+}
